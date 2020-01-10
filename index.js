e9df4db8ab5df5b3155a041648e862b8f9618dbd
lpdft <772346730@qq.com>
2020-01-10 17:44:29 +0800

    [ruyue] feat-2-AC4: render project list user db.json datas

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const API_ROOT = "http://localhost:3000/projects";
 let taskList = document.getElementsByTagName('tbody')[0];
 getListData();
 
-let status = document.getElementsByClassName('status');
-changeColorByStatus(status);
+
 
 function getListData() {
   ajax({
       url: API_ROOT,
       method: "GET",
       success: function(responseText) {
-          console.log(responseText);
+        renderProjectList(responseText);
+        let status = document.getElementsByClassName('status');
+        changeColorByStatus(status);
       }, 
   })
 }
@@ -30,5 +31,21 @@ function changeColorByStatus(status) {
           default:
       }
   }
+}
+
+function renderProjectList(data) {
+  if (!Array.isArray(data) && !data instanceof Array) {
+      return false;
+  }
+  taskList.innerHTML = data.reduce((acc, cur) => {
+      return acc += `
+      <tr data-id='${cur.id}'>
+          <td>${cur.name}</td>
+          <td class="description">${cur.description}</td>
+          <td>${cur.endTime}</td>
+          <td class = 'status'>${cur.status}</td>
+          <td><input type="submit" value="删除" class="delete"></td>
+      </tr>`;
+  },'');
 }
 
